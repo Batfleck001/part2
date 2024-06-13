3c89fa2e9d9abc0c75807a73db251afb4f0b2af4
Batfleck001 <harishsekar1423@gmail.com>
2024-06-13 19:54:28 +0530

    Done with 2.11

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,16 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Number from './components/Number'
 
 const App = () => {
 
-  const [persons, setpersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setpersons] = useState([])
+
+  useEffect(()=>{
+    axios
+    .get("http://localhost:3001/persons")
+    .then(res => setpersons(res.data))
+    .catch(e => console.log("Error :",e))
+  },[])
   
   return (
     <>
